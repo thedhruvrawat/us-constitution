e639e6a26a3e96d8f30653e5950534f3089308f7
sarang sridhar <sare.sri01@gmail.com>
2021-10-16 12:16:35 +0530

    article 7 second half and ammendment

--- src/article7.pl
+++ src/article7.pl
@@ -10,3 +10,40 @@ signatories('Virginia', 'John Blair,James Madison Jr. ').
 signatories('North Carolina', 'Wm. Blount,Richd. Dobbs Spaight,Hu Williamson ').
 signatories('South Carolina', 'J. Rutledge,Charles Cotesworth Pinckney,Charles Pinckney,Pierce Butler ').
 signatories('Georgia', 'William Few,Abr Baldwin ').
+
+letterOfRatification:- write('In Convention Monday
+    September 17th, 1787.
+    Present
+    The States of
+    New Hampshire, Massachusetts, Connecticut, Mr. Hamilton from New York, New Jersey, Pennsylvania, Delaware,
+    Maryland, Virginia, North Carolina, South Carolina and
+    Georgia.
+    Resolved,
+    That the preceeding Constitution be laid before the United
+    States in Congress assembled, and that it is the Opinion
+    of this Convention, that it should afterwards be submitted
+    to a Convention of Delegates, chosen in each State by the
+    People thereof, under the Recommendation of its Legislature, for their Assent and Ratifi cation; and that each Convention assenting to, and ratifying the Same, should give
+    Notice thereof to the United States in Congress assembled.
+    Resolved, That it is the Opinion of this Convention, that
+    as soon as the Conventions of nine States shall have ratifi ed
+    this Constitution, the United States in Congress assembled
+    should fi x a Day on which Electors should be appointed by
+    the States which shall have ratifi ed the same, and a Day on
+    which the Electors should assemble to vote for the President, and the Time and Place for commencing Proceedings
+    under this Constitution.
+    That after such Publication the Electors should be appointed, and the Senators and Representatives elected: That
+    the Electors should meet on the Day fi xed for the Election
+    of the President, and should transmit their Votes certifi ed,
+    signed, sealed and directed, as the Constitution requires, to
+    the Secretary of the United States in Congress assembled,
+    that the Senators and Representatives should convene at the
+    Time and Place assigned; that the Senators should appoint
+    a President of the Senate, for the sole Purpose of receiving,
+    opening and counting the Votes for President; and, that
+    after he shall be chosen, the Congress, together with the
+    President, should, without Delay, proceed to execute this
+    Constitution.
+    By the unanimous Order of the Convention
+    Go. Washington-Presidt:
+    W. JACKSON Secretary.').
